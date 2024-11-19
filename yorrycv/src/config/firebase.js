50829854878e7ae23264cf17daad893015d821a5
yorry69 <s22210690@student.unklab.ac.id>
2024-11-20 01:59:17 +0800

    instalasi firebase

---
+++ yorrycv/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsUt_CT8aE5B1bAIWYuftVWQEoqUtt-JM",
+  authDomain: "midproject-c7080.firebaseapp.com",
+  projectId: "midproject-c7080",
+  storageBucket: "midproject-c7080.firebasestorage.app",
+  messagingSenderId: "875599830314",
+  appId: "1:875599830314:web:777c288b54bcb06f4fd253"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
